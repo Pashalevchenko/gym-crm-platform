dd9fe0b5b80dd6c87c87b9ea34d03edfc3d747eb
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-06-12 13:15:45 +0300

    GIA-229 Implement Workload microservice - add models

--- workload-service/src/test/java/gym/crm/platform/workload/WorkloadServiceTests.java
+++ workload-service/src/test/java/gym/crm/platform/workload/WorkloadServiceTests.java
@@ -3,14 +3,16 @@ package gym.crm.platform.workload;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
-public class WorkloadServiceTests {
+class WorkloadServiceTests {
 
     @Test
     void contextLoads() {}
 
     @Test
     void mainRuns() {
-        WorkloadService.main(new String[]{});
+        assertDoesNotThrow(() -> WorkloadService.main(new String[]{}));
     }
 }
