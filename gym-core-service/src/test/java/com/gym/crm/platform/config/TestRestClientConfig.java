8d504c830c95da0d38d2c1dcfbe3fd9d099ad3ce
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-06-17 13:52:36 +0300

    GIA-230 Integrate gia-core-service with workload-service

--- gym-core-service/src/test/java/com/gym/crm/platform/config/TestRestClientConfig.java
+++ gym-core-service/src/test/java/com/gym/crm/platform/config/TestRestClientConfig.java
@@ -3,9 +3,11 @@ package com.gym.crm.platform.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestClient;
 
 @TestConfiguration
+@PropertySource(value = "classpath:application-test.yml", factory = YamlPropertySourceFactory.class)
 public class TestRestClientConfig {
 
     @Value("${workload.service.base-url}")
