290b56b58aa06c50e81adda004058a5a869fcd9b
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-06-18 22:12:46 +0300

    GIA-231 Implement discovery server

--- discovery-server/src/test/java/com/gym/crm/platform/discovery/DiscoveryServerTest.java
+++ discovery-server/src/test/java/com/gym/crm/platform/discovery/DiscoveryServerTest.java
@@ -4,18 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 @SpringBootTest
 @ActiveProfiles("test")
 class DiscoveryServerTest {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void mainRuns() {
-        assertDoesNotThrow(() -> DiscoveryServer.main(new String[]{}));
     }
 }
