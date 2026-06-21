eaafea49cb8cb55e237551fc8db93f28b4a68c40
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-06-21 23:10:45 +0300

    GIA-234 Implement microservices authorization

--- workload-service/src/test/java/gym/crm/platform/workload/controller/TrainerWorkloadControllerTest.java
+++ workload-service/src/test/java/gym/crm/platform/workload/controller/TrainerWorkloadControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.time.LocalDate;
 
 import static org.mockito.Mockito.verify;
